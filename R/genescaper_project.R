a52d3a039e36d1cb68af40a611a8eadaafa90b9a
Bob Verity <bobverity1@gmail.com>
2022-02-15 14:16:37 +0000

    added bind_data function

---
+++ R/genescaper_project.R
@@ -0,0 +1,72 @@
+
+#------------------------------------------------
+#' @title Define new GeneScapeR project
+#'
+#' @description TODO
+#'
+#' @export
+
+genescaper_project <- function() {
+  
+  # create empty project
+  project <- list(data = NULL,
+                  model = NULL,
+                  maps = NULL,
+                  pairwise_predictions = NULL,
+                  distance_predictions = NULL,
+                  pMAPI = NULL)
+  
+  class(project) <- "genescaper_project"
+  
+  # return
+  invisible(project)
+}
+
+#------------------------------------------------
+# overload print() function
+#' @method print genescaper_project
+#' @export
+print.genescaper_project <- function(x, ...) {
+  
+  # print summary
+  summary(x)
+  
+  # return invisibly
+  invisible(x)
+}
+
+#------------------------------------------------
+# overload summary() function
+#' @method summary genescaper_project
+#' @export
+summary.genescaper_project <- function(object, ...) {
+  p <- object
+  
+  # if empty project
+  if (all(mapply(is.null, p))) {
+    message("(empty project)")
+    invisible(object)
+  }
+  
+  # print data properties
+  if (!is.null(p$data)) {
+    message("Data: (todo)")
+    #n_demes <- length(p$epi_model_parameters$H)
+    #message(sprintf("  demes: %s", n_demes))
+    #message(sprintf("  H:\t %s", paste(p$epi_model_parameters$H, collapse = ", ")))
+    #message(sprintf("  M:\t %s", paste(p$epi_model_parameters$M, collapse = ", ")))
+    #message(sprintf("  seed infections: %s", paste(p$epi_model_parameters$seed_infections, collapse = ", ")))
+    
+    message("")
+  }
+  
+  # print model parameters
+  if (!is.null(p$model)) {
+    message("Model: (todo)")
+    
+    message("")
+  }
+  
+  invisible(object)
+}
+
--- R/main.R
+++ R/main.R
@@ -1,31 +1,68 @@
 #------------------------------------------------
-#' @title Square a vector of values
+#' @title Load data into project
 #'
-#' @description Simple test function that demonstrates some of the features of
-#'   this package by squaring an input vector of values.
+#' @description TODO
 #'
-#' @param x vector of values.
+#' @param project a genescaper project, as produced by the
+#'   \code{genescaper_project()} function.
+#' @param site_data TODO
+#' @param genetic_data TODO
 #'
+#' @importFrom rlang .data
 #' @export
-#' @examples
-#' # Find square of first 100 values
-#' square(1:100)
 
-square <- function(x = 1:5) {
+bind_data <- function(project, site_data, genetic_data) {
   
-  # print message to console
-  message("running R square function")
+  # check inputs
+  assert_class(project, "genescaper_project")
   
-  # get arguments in list form
-  args <- list(x = x)
+  # check format of site_data
+  assert_dataframe(site_data)
+  assert_in(c("site_ID", "latitude", "longitude"), names(site_data),
+            message = "site_data must have columns {site_ID, latitude, longitude}")
+  assert_vector_numeric(site_data$latitude)
+  assert_bounded(site_data$latitude, left = -90, right = 90)
+  assert_vector_numeric(site_data$longitude)
+  assert_bounded(site_data$longitude, left = -180, right = 180)
   
-  # run C++ function with these arguments
-  output_raw <- square_cpp(args)
+  # check format of genetic_data
+  assert_dataframe(genetic_data)
+  assert_in(c("site_ID", "locus", "allele", "freq"), names(genetic_data),
+            message = "genetic_data must have columns {site_ID, locus, allele, freq}")
+  assert_vector_pos_int(genetic_data$locus)
+  assert_vector_pos_int(genetic_data$allele)
+  assert_vector_pos(genetic_data$freq)
+  assert_bounded(genetic_data$freq, inclusive_left = FALSE, inclusive_right = FALSE,
+                 message = "allele frequencies must be in the range (0, 1), and cannot equal exactly 0 or 1")
   
-  # some optional processing of output
-  message("processing output")
-  ret <- output_raw$x_squared
+  # check that site_IDs match between datasets
+  assert_eq(sort(unique(site_data$site_ID)),
+            sort(unique(genetic_data$site_ID)),
+            message = "site_data and genetic_data must contain the same set of site_ID values")
   
-  # return
-  return(ret)
+  # check that all loci are represented in all sites and alleles
+  locus_match <- genetic_data %>%
+    dplyr::group_by(.data$site_ID, .data$allele) %>%
+    dplyr::summarise(hash = rlang::hash(.data$locus),
+                     .groups = "drop_last")
+  if (length(unique(locus_match$hash)) != 1) {
+    stop("the same set of loci (in the same order) must be represented for all site_ID & allele combinations.")
+  }
+  
+  # check that the same alleles are represented in all sites and loci
+  allele_match <- genetic_data %>%
+    dplyr::group_by(.data$site_ID, .data$locus) %>%
+    dplyr::summarise(hash = rlang::hash(.data$locus),
+                     .groups = "drop_last") %>%
+    dplyr::group_by(.data$locus) %>%
+    dplyr::summarise(same_alleles = length(unique(.data$hash)) == 1)
+  if (!all(allele_match$same_alleles)) {
+    stop("for a given locus, the same set of alleles (in the same order) must be represented for all site_IDs.")
+  }
+  
+  # load data into project
+  project$data$raw <- list(site_data = site_data,
+                           genetic_data = genetic_data)
+  
+  return(project)
 }
